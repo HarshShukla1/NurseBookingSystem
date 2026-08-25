e930d4ed3f47061df7328f7edc36df80e68c496b
Harsh Shukla <harshshukla24112000@gmail.com>
2026-08-25 15:23:53 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/harsh/nursebookingsystem/NurseBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.harsh.nursebookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NurseBookingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NurseBookingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/harsh/nursebookingsystem/NurseBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harsh.nursebookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NurseBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
